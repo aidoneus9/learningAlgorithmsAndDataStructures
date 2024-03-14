cf9d3865a38ce6e16fc74bdbb3eec8417ce2a94c
aidoneus9 <155718337+aidoneus9@users.noreply.github.com>
2024-03-14 22:18:34 +0900

    final commit 🚀

--- chunk/index.js
+++ chunk/index.js
@@ -12,6 +12,7 @@
 
 // <29. Chunk Solution #1>
 
+// <30. More on Chunk>
 function chunk(array, size) {
   const chunked = [];
 
@@ -24,6 +25,26 @@ function chunk(array, size) {
       last.push(element);
     }
   }
+
+  return chunked;
+}
+
+module.exports = chunk;
+
+// <31. Chunk Solution #2>
+const letters = ["a", "b", "c", "d", "e"];
+letters.slice(0, 3); // ['a', 'b', 'c']
+
+// <32. Even More on Chunk!>
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+
   return chunked;
 }
 
