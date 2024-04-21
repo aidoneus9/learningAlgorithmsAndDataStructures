e7e89ea54a9e06af49c0502355beb93f03495fc4
aidoneus9 <155718337+aidoneus9@users.noreply.github.com>
2024-04-22 01:12:50 +0900

    second commit 🚀

--- anagrams/index.js
+++ anagrams/index.js
@@ -16,9 +16,55 @@ const word = "HI THERE!!!!!";
 word.replace(/[^\w]/g, "").toLowerCase(); // hithere
 
 // 💡 generate a character map out of both the strings that you are given.
-// edge case: 2 ways of addressing the issue
-// count the number of keys that are present inside of one object and compare it to the keys that exist inside the other
-// count the length of characters inside this string and compare it to the other
-function anagrams(stringA, stringB) {}
+// ⚠️ edge case: if we only compare this object to this one, we might miss out on some extra entries inside the second one
+// 👉 2 ways of addressing the issue
+// 1. count the number of keys that are present inside of one object and compare it to the keys that exist inside the other ✔️
+// 2. count the length of characters inside this string and compare it to the other
+
+// for the edge case: we're going to pull out all the keys from both these objects, and we'll count the number of keys that is contained in both
+
+// ✍️ how to pull out the keys inside of an object
+const obj = {
+  a: 1,
+  b: 1,
+  c: 1,
+};
+
+Object.keys(obj); // ['a', 'b', 'c']
+Object.keys(obj).length; // 3
+
+// <34. Solving Anagrams>
+function anagrams(stringA, stringB) {
+  // Build a character map
+  const aCharMap = buildCharMap(stringA);
+  const bCharMap = buildCharMap(stringB);
+
+  // for the edge case
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    return false;
+  }
+  // Compare all the characters
+  // iterating over an...
+  // array: OF // object: IN
+  for (let char in aCharMap) {
+    if (aCharMap[char] !== bCharMap[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// Helper function
+function buildCharMap(str) {
+  const charMap = {};
+
+  for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+    // assign a key to the character map of the current character that we're looking at, and then we increment the value at that character
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  return charMap;
+}
 
 module.exports = anagrams;
