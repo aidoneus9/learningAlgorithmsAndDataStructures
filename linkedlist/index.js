f99bd3f220c76974ae250aceb9ba6dc636b32ea6
aidoneus9 <155718337+aidoneus9@users.noreply.github.com>
2024-05-13 23:38:14 +0900

    first commit 🚀

---
+++ linkedlist/index.js
@@ -0,0 +1,21 @@
+// --- Directions
+// Implement classes Node and Linked Lists
+// See 'directions' document
+
+// <71. What's a Linked List?>
+// link list: an ordered collection of data(The collection contains a number of different nodes)
+const nodeOne = {
+  data: 123,
+};
+
+const nodeTwo = {
+  data: 456,
+};
+
+nodeOne.next = nodeTwo;
+
+class Node {}
+
+class LinkedList {}
+
+module.exports = { Node, LinkedList };
