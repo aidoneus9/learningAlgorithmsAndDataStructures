005263c7434b2a578bbebe2df4dc49f9b9e7c5b5
aidoneus9 <155718337+aidoneus9@users.noreply.github.com>
2024-06-17 18:35:20 +0900

    11th commit ✍️

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -477,7 +477,6 @@ class LinkedList {
     previous.next = null;
   }
 }
-*/
 
 // <89. A new Tail to Tell with InsertLast>
 // <90. Adding InsertLast>
@@ -575,5 +574,116 @@ class LinkedList {
     }
   }
 }
+*/
+
+// <91. Pick Em Out with GetAt>
+// <92. GetAt Solution>
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  size() {
+    let counter = 0;
+    let node = this.head;
+
+    while (node) {
+      counter++;
+      node = node.next;
+    }
+
+    return counter;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    if (!this.head) {
+      return null;
+    }
+
+    let node = this.head;
+    while (node) {
+      if (!node.next) {
+        return node;
+      }
+      node = node.next;
+    }
+
+    // let node = this.head;
+    // while (node.next) node = node.next;
+    // return node;
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  removeFirst() {
+    if (!this.head) {
+      return;
+    }
+
+    this.head = this.head.next;
+  }
+
+  removeLast() {
+    if (!this.head) {
+      return;
+    }
+
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+
+    let previous = this.head;
+    let node = this.head.next;
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+    previous.next = null;
+  }
+
+  insertLast(data) {
+    const last = this.getLast();
+
+    if (last) {
+      last.next = new Node(data);
+    } else {
+      this.head = new Node(data);
+    }
+  }
+
+  getAt(index) {
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+
+      counter++;
+      node = node.next;
+    }
+    return null;
+  }
+}
 
 module.exports = { Node, LinkedList };
+
+// 🤔 GetLast GetAt
