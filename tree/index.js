9cd33d80ee4e5ee460df4ca364c2ea036f6ea71e
aidoneus9 <155718337+aidoneus9@users.noreply.github.com>
2024-06-25 18:21:36 +0900

    first commit 🚀

---
+++ tree/index.js
@@ -0,0 +1,24 @@
+// --- Directions
+// 1) Create a node class.  The constructor
+// should accept an argument that gets assigned
+// to the data property and initialize an
+// empty array for storing children. The node
+// class should have methods 'add' and 'remove'.
+// 2) Create a tree class. The tree constructor
+// should initialize a 'root' property to null.
+// 3) Implement 'traverseBF' and 'traverseDF'
+// on the tree class.  Each method should accept a
+// function that gets called with each element in the tree
+
+// <108. Tree Overview>
+// 📌 tree traversal: iterating through all the different elements within a tree
+// -> 2 distinct ways in which we order the elements and iterate through those elements
+// 1) Breadth-First Traversal
+// 2) Depth-Frist Traversal: essentially with Depth-Frist Traversal, we try to get to the bottom of the tree as quickly as possible.
+
+//
+class Node {}
+
+class Tree {}
+
+module.exports = { Tree, Node };
