05ad0ad71313f94f44a613b7dccc85b2fee5ed5e
aidoneus9 <155718337+aidoneus9@users.noreply.github.com>
2024-06-27 19:15:32 +0900

    3rd commit ✍️

--- tree/index.js
+++ tree/index.js
@@ -34,6 +34,12 @@ class Node {
     // this.children.push(node);
     this.children.push(new Node(data));
   }
+
+  remove(data) {
+    this.children.filter((node) => {
+      return node.data !== data;
+    });
+  }
 }
 
 class Tree {}
