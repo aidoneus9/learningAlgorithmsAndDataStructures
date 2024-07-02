867534ecfca16161110b36702ed90a3025ec3614
aidoneus9 <155718337+aidoneus9@users.noreply.github.com>
2024-07-02 14:12:42 +0900

    seventh commmit ✍️

--- tree/index.js
+++ tree/index.js
@@ -9,6 +9,7 @@
 // 3) Implement 'traverseBF' and 'traverseDF'
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
+// -> ✍️ make sure you accept a function that gets called with every node in the tree
 
 // <108. Tree Overview>
 // 📌 tree traversal: iterating through all the different elements within a tree
@@ -58,14 +59,29 @@ class Node {
   }
 }
 
-/*
 // <111. Tree Implementation>
+// <112. Traverse By Breadth>
+// <113. Solving for Breadth-First Traversal>
 class Tree {
   constructor() {
     this.root = null; // when we first create a tree, it's going to start off with an empty root properly
   }
+
+  traverseBF(fn) {
+    // 📌 take the root node out of our tree and stick it into a brand new array
+    const arr = [this.root];
+    while (arr.length) {
+      const node = arr.shift();
+
+      // 📌 immediately take all of this node's children and push them into the array
+      arr.push(...node.children);
+      // for (let child of node.children) {
+      //   arr.push(child);
+      // }
+      fn(node); // 🤔
+    }
+  }
 }
-// ✍️ Now, one thing that you'll notice about this tree implementation is that we did not add on, and we're not saying that we're going to add on any type of insert or remove methods to this thing like we had done previously with the linked list; with the linked list, the linked list class was really in charge of all the elements inside the linked list. Yes, the linked list had references to each subsequent element where each node had references to each subsequent element. But if we called add or remove on that thing on the linked list, it was more or less kind of obvious how we wanted to operate on the linked list. But in the case of a tree, whenever we start to want to add or remove elements from it, we have to very precisely specify which node we want to be adding or removing elements from.
 
 const node = new Node(1); // const node would be new node with some amount of data
 const tree = new Tree(); // then to create a tree and
@@ -73,17 +89,7 @@ tree.root = node; // then we would manually update the root property of the tree
 // -> so now the tree thinks that its root node is the node we just created
 
 module.exports = { Tree, Node };
-*/
-
-// <112. Traverse By Breadth>
-class Tree {
-  constructor() {
-    this.root = null;
-  }
-}
-
-const node = new Node(1);
-const tree = new Tree();
-tree.root = node;
-
-module.exports = { Tree, Node };
+// (68) ✍️ Now, one thing that you'll notice about this tree implementation is that we did not add on, and we're not saying that we're going to add on any type of insert or remove methods to this thing like we had done previously with the linked list; with the linked list, the linked list class was really in charge of all the elements inside the linked list. Yes, the linked list had references to each subsequent element where each node had references to each subsequent element. But if we called add or remove on that thing on the linked list, it was more or less kind of obvious how we wanted to operate on the linked list. But in the case of a tree, whenever we start to want to add or remove elements from it, we have to very precisely specify which node we want to be adding or removing elements from.
+// (73) as long as array.length is a truthy value; so as soon as our array is empty, as soon as it has a length of zero, that means there's nothing else for us to do
+// (74) shift() method will take out the first element of the array
+// (77) ⚠️ Now the important thing to keep in mind is that we cannot say simply node.children because node.children is an array; so if we push an array into an array, we would then have a nested array
