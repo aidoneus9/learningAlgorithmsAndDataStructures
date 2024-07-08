f60c068568556c0fdcc9804bd44d30fd1adbbadf
aidoneus9 <155718337+aidoneus9@users.noreply.github.com>
2024-07-08 22:06:32 +0900

    first commit ✍️

---
+++ levelwidth/index.js
@@ -0,0 +1,25 @@
+// --- Directions
+// Given the root node of a tree, return
+// -> ⚠️ We are working with a node here(📎 node.js file: this is an individual node that we're working with. There is no tree class.). (16) root is a node.
+// an array where each element is the width
+// of the tree at each level.
+// -> ⚠️ width: breadth first traversal
+// --- Example
+// Given:
+//     0
+//   / |  \
+// 1   2   3
+// |       |
+// 4       5
+// Answer: [1, 3, 2]
+
+// <116. Level Width Declaration>
+// ⚠️ You are not going to be using some pre-built traversal function. Instead, you are going to have to write a function that will traverse through the tree and do some work during that traversal.
+function levelWidth(root) {}
+
+module.exports = levelWidth;
+
+// <117. Measuring Level Width>
+function levelWidth(root) {}
+
+module.exports = levelWidth;
