a5b92ba7f5ac217822e097e75fe978de6c2215e3
aidoneus9 <155718337+aidoneus9@users.noreply.github.com>
2024-07-10 23:51:54 +0900

    final commit 🚀

--- levelwidth/index.js
+++ levelwidth/index.js
@@ -19,21 +19,21 @@
 // <117. Measuring Level Width>
 function levelWidth(root) {
   const arr = [root, "s"];
-  const counters = [0];
+  const widths = [0];
 
   while (arr.length > 1) {
     const node = arr.shift();
 
     if (node === "s") {
-      counters.push(0);
+      widths.push(0);
       arr.push("s");
     } else {
       arr.push(...node.children);
-      counters[counters.length - 1]++;
+      widths[widths.length - 1]++;
     }
   }
 
-  return counters;
+  return widths;
 }
 
 module.exports = levelWidth;
