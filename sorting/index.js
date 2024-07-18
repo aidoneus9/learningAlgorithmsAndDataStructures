8ce7a7d59a95136edba0af43235cdcc5a3b493ab
aidoneus9 <155718337+aidoneus9@users.noreply.github.com>
2024-07-18 15:41:38 +0900

    first commit 🚀

---
+++ sorting/index.js
@@ -0,0 +1,25 @@
+// --- Directions
+// Implement bubbleSort, selectionSort, and mergeSort
+
+// <130. Sorting Algorithm Overview>
+// <131. BubbleSort Implementation>
+// ✍️ its purpose is to find the largest element in the array and drag it over to the far right hand side
+// 📌 pseudocode
+// From i=0 to < array length
+//   From j=0 to (array length-i)
+// ->  anytime that you see nested for loops, iterating over the same collection of data, that's an automatic red flag that we might have n^2 runtime complexity
+//     If the element at j is greater than j+1
+//       Swap elements at j and j+1
+
+function bubbleSort(arr) {
+  // Implement bubblesort
+  // return the sorted array
+}
+
+function selectionSort(arr) {}
+
+function mergeSort(arr) {}
+
+function merge(left, right) {}
+
+module.exports = { bubbleSort, selectionSort, mergeSort, merge };
