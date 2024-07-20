54395f042e5085e46b3792a168babfc5202151f3
aidoneus9 <155718337+aidoneus9@users.noreply.github.com>
2024-07-20 17:21:54 +0900

    3rd commit ✍️

--- sorting/index.js
+++ sorting/index.js
@@ -27,14 +27,49 @@ function bubbleSort(arr) {
   // return the sorted array
   return arr;
 }
+// (17) With for of, we don't get easy access to the index of individual elements. The for of loop is really for taking some type of operation on EACH INDIVIDUAL ELEMENT itslef. And we don't really care about the elements so much as we do the indexes, or at least we care about them equally in this case.
+// -> Use a classic for loop so that we can easily get access to both the index and element at every position in the array.
 
-function selectionSort(arr) {}
+// <133. How SelectionSort Works>
+// <134. Selection Sort Solution>
+// -> prove me wrong algorithm
+// 📌 pseudocode
+// From i=0 to < array length
+//   ASSUME the element at 'i' is the least in the array, assign i to 'indexOfMin'
+// -> ✍️ Assume that the current element at i is the lowest in the array and prove me wrong. Show me if there is some other value in the array with a lower value.
+// -> ✍️ the entire purpose of this inner for loop is to somehow invalidate our assumption right here is to tell us, "no, you couldn't assume that, that was not a good assumption, you have to swap the two elements".
+//   For j from i+1 to end of array
+//     See if there is an element with lower value
+//       If there is, record its index
+//   If the index of the current element and the index of the 'lowest' element is not the same, swap them
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    // Assumption
+    let indexOfMin = i;
+
+    // Validate that assumption
+    for (let j = j + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
+
+    if (indexOfMin !== i) {
+      // Swap
+      let lesser = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser;
+      // swap(arr, indexOfMin, i)
+    }
+  }
+
+  return arr;
+}
+// (55) the indexOfMin is going to point to the absolute lowest value right now
+// (55) ~ (57) if we put together a function to encapsulate the swapping logic right here, and if we somehow turn this thing into just swap array at indexOfMin and i or whatever we want to do, that would turn this into a very brief little solution right here(58)
 
 function mergeSort(arr) {}
 
 function merge(left, right) {}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
-
-// (17) With for of, we don't get easy access to the index of individual elements. The for of loop is really for taking some type of operation on EACH INDIVIDUAL ELEMENT itslef. And we don't really care about the elements so much as we do the indexes, or at least we care about them equally in this case.
-// -> Use a classic for loop so that we can easily get access to both the index and element at every position in the array.
